f346153002186831eeea8895218ff09b3075f289
amdsantos <amd.mantovani@gmail.com>
2022-09-17 23:23:10 -0300

    Desenvolvimento das atividades

--- modulo6/lixo.py
+++ modulo6/lixo.py
@@ -1,9 +1,28 @@
 
 
-class Utilitarios:
+# class Utilitarios:
 
-    def __init__(qualquevalorpodeserinformadoaqui):
-        qualquevalorpodeserinformadoaqui.x = 1
+#     def __init__(qualquevalorpodeserinformadoaqui):
+#         qualquevalorpodeserinformadoaqui.x = 1
 
-u = Utilitarios()
-print(u.x)
+# u = Utilitarios()
+# print(u.x)
+
+
+
+def compararString(a, b):
+    i = 0
+    while i < len(a):
+        if a[i] != b[i]:
+            return False
+            break
+        i += 1
+    return True
+
+#
+# Seu teste
+#
+cor1 = "laranja"
+cor2 = "amarelo"
+print(compararString(cor1, cor2))
+assert compararString(cor1, cor1)

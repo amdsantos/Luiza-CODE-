d977ebc2a115f81c0cf44f075d99a62dc3636879
amdsantos <amd.mantovani@gmail.com>
2022-09-21 21:14:30 -0300

    MongoDB

--- modulo7/src/models/address.py
+++ modulo7/src/models/address.py
@@ -1,10 +1,10 @@
 import user
-import address
+
 
 
 USER = (user.find({'id': {id}}))
 
-address_user = address.find({'user._id': USER[id]})
+address_user = address_collection.find({'user._id': USER[id]})
 
 async def create_address(address_collection):
     
@@ -12,7 +12,7 @@ async def create_address(address_collection):
         address_user == None
         
         if user.iserted_address:
-            address_user =  await address_collection.insert_one({'user': {user, 'address': {address}}})
+            address_user =  await address_collection.insert_one({'user': {user}, 'address': {address_user}})
         return address_user
 
     except Exception as e:
